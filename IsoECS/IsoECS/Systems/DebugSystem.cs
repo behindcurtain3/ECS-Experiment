80388fe0478caccb72fea9f7f9757338fa35861e
Justin Brown <behindcurtain3@gmail.com>
2014-10-30 23:47:23 -0700

    Improved DebugSystem.

--- IsoECS/IsoECS/Systems/DebugSystem.cs
+++ IsoECS/IsoECS/Systems/DebugSystem.cs
@@ -12,11 +12,11 @@ namespace IsoECS.Systems
 {
     public class DebugSystem : ISystem
     {
+        private List<Entity> brains = new List<Entity>();
+
         public void Update(int dt)
         {
-            List<Entity> citizens = EntityManager.Instance.Entities.FindAll(delegate(Entity e) { return e.HasComponent<CitizenComponent>(); });
-
-            foreach (Entity e in citizens)
+            foreach (Entity e in brains)
             {
                 DrawableComponent drawable = e.Get<DrawableComponent>();
                 CitizenComponent citizen = e.Get<CitizenComponent>();
@@ -43,6 +43,21 @@ namespace IsoECS.Systems
 
         public void Init()
         {
+            brains.AddRange(EntityManager.Instance.Entities.FindAll(delegate(Entity e) { return e.HasComponent<CitizenComponent>(); }));
+
+            EntityManager.Instance.EntityAdded += new EntityManager.EntityEventHandler(Instance_EntityAdded);
+            EntityManager.Instance.EntityRemoved += new EntityManager.EntityEventHandler(Instance_EntityRemoved);
+        }
+
+        void Instance_EntityRemoved(Entity e)
+        {
+            brains.Remove(e);
+        }
+
+        void Instance_EntityAdded(Entity e)
+        {
+            if (e.HasComponent<CitizenComponent>())
+                brains.Add(e);
         }
 
         public void Shutdown()
