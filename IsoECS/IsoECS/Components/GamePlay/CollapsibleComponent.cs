e99273b287958cc615d03706bbb82fdfe463491c
Justin Brown <behindcurtain3@gmail.com>
2014-11-09 21:22:02 -0800

    Added CollapsibleComponent.

---
+++ IsoECS/IsoECS/Components/GamePlay/CollapsibleComponent.cs
@@ -0,0 +1,11 @@
+﻿using TecsDotNet;
+
+namespace IsoECS.Components.GamePlay
+{
+    public class CollapsibleComponent : Component
+    {
+        // 0 = collapsed, 100 = maxxed out
+        // reduces over time
+        public double Status { get; set; }
+    }
+}
--- IsoECS/IsoECS/GamePlay/Services.cs
+++ IsoECS/IsoECS/GamePlay/Services.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using TecsDotNet;
 using IsoECS.DataStructures;
+using Microsoft.Xna.Framework;
 
 namespace IsoECS.GamePlay
 {
